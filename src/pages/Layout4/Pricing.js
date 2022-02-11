ec178263dcd8de09cde9c5064c1e079b091aedd0
pranavbaradkar <pyb11@iitbbs.ac.in>
2022-02-12 02:08:08 +0530

    init

--- src/pages/Layout4/Pricing.js
+++ src/pages/Layout4/Pricing.js
@@ -74,35 +74,6 @@ export default class Pricing extends Component {
               <Row className = "mt-5 pt-4"> 
               {this.state.pricing.map((item, key) => (
                 <Col lg = "4" key={key}>
-                  {/* <div className="pricing-box mt-4">
-                    {item.pricingTag && (
-                      <div className="pricing-badge"><span className="badge">Featured</span> </div>
-                    )}
-
-                    <i className={"h1 mdi " + item.icon}></i>
-                    {(item.titlePrimary === true) ?
-                      <h4 className="f-20 text-primary">{item.title}</h4>
-                      :
-                      <h4 className="f-20">{item.title}</h4>
-                    }
-
-                    <div className="mt-4 pt-2">
-                      <p className="mb-2 f-18">Features</p>
-                      {item.child.map((linkItem, linkkey) => (
-                        <p className="mb-2" key={linkkey}><i
-                          className={"mdi " + linkItem.icon + " f-18 mr-2"}></i><b>{linkItem.btitle}</b> {linkItem.title}</p>
-                      ))}
-                    </div>
-                    <p className="mt-4 pt-2 text-muted">{item.description}
-                    </p>
-                    <div className="pricing-plan mt-4 pt-2">
-                      <h4 className="text-muted"><s> {item.regularprice}</s> <span className="plan pl-3 text-dark">{item.saleprice}</span></h4>
-                      <p className="text-muted mb-0">Per Month</p>
-                    </div>
-                    <div className="mt-4 pt-3">
-                      <Link to="#" className="btn btn-primary btn-rounded">Read More</Link>
-                    </div>
-                  </div> */}
                   <Card>
                     <Card.Img variant="top" src={item.img} />
                     <Card.Body>
